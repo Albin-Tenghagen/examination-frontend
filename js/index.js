282c30f027a0ff0dc5a73a6394ea8c450ae5d4e4
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2024-12-29 14:38:02 +0100

    Ovject creation and an array for manipulating the movie objects with localstorage has begun, The basic logic behind how it should work is done. Now i Have to get it to work properly so the object get pushed to localStorage, And displays on the page in the watchlist section

--- js/index.js
+++ js/index.js
@@ -3,13 +3,54 @@ console.log('JavaScript file loaded correctly')
 //* Global Variables down below
 const itemsPerPage = 12;
 let currentPage = 1;
+
 const apiUrl = "https://api.themoviedb.org/3?"
 const exploringEndpoint = "discover/movie"
 const apiKey = "3cb0d2bc09efade109b0b6a67290e815"
+
 let movieArray = [];
+
+let objectManipulationArray = [];
+
 let spotlightArray = [];
+
 let movieWatchListArray = [];
 
+const movieObject = {
+    movieObjectId: undefined,
+    movieObjectTitle: "",
+    movieObjectOverview: "",
+    movieObjectImg: undefined,
+    movieObjectRelease: ""
+}
+
+function objectCreation(movie){
+    const generalMovieObject = Object.create(movieObject)
+    generalMovie.movieObjectId = 
+    generalMovie.movieObjectTitle = movie.original_title
+    generalMovie.movieObjectOverview = movie.overview
+    generalMovie.movieObjectImg = movie.poster_path
+    generalMovie.movieObjectRelease = movie.release_date
+
+    objectManipulationArray.push(generalMovieObject)
+
+}
+console.log("objectManipulationArray", objectManipulationArray)
+function localStorageAddition(){
+    const watchlistedMovie = Object.create(movieObject);
+
+    watchlistedMovie.movieObjectId = 
+    watchlistedMovie.movieObjectTitle = 
+    watchlistedMovie.movieObjectOverview = 
+    watchlistedMovie.movieObjectImg = 
+    watchlistedMovie.movieObjectRelease = ""
+
+    
+}
+
+function displayWatchlist(){
+
+}
 //*---------------
 
 //*   DOM Creation
@@ -18,8 +59,7 @@ const spotlightSection = document.getElementById("spotlightSection");
 const watchListContainer = document.getElementsByClassName("watchListContainer");
 
 //*---------------
-
-movieApiFetch()
+//! movieApiFetch()
 
 async function movieApiFetch() {
     console.log("fetching data from OMDB API")
@@ -34,6 +74,9 @@ async function movieApiFetch() {
         const movieData = await response.json();
         
         movieArray = movieData.results;
+        movieArray.forEach(movie => {
+            objectCreation(movie)
+        })
         console.log(movieArray)
 
         spotlightArray = movieArray.slice(0, 10)
@@ -78,35 +121,36 @@ async function movieApiFetch() {
 function createMovieObject(movie){
     const movieContainer = document.createElement("article")
     movieContainer.setAttribute("class", "movieContainer")
+    //TODO FIX SO THE FUNCTION CAN PLACE THE ARTICLES IN DIFFERENT NODES
     spotlightSection.appendChild(movieContainer)
 
 
-    //TODO original_title
+    //* original_title
     const movieTitle = document.createElement("h4")
     movieTitle.textContent = movie.original_title
     movieTitle.setAttribute("class", "movieTitle")
     movieContainer.appendChild(movieTitle)
 
-    //TODO Save to localStorage button
+    //* Save to localStorage button
     const watchlistButton = document.createElement("button")
     watchlistButton.setAttribute("class", "watchlistButton")
     watchlistButton.textContent = "Add to Watchlist"
     movieContainer.appendChild(watchlistButton)
 
-    //TODO backdrop_path 
+    //* backdrop_path 
     const movieImg = document.createElement("img") 
     movieImg.setAttribute("src", `https://image.tmdb.org/t/p/w500${movie.poster_path}`)
     movieImg.setAttribute("alt", movie.original_title)
     movieImg.setAttribute("class", "movieImg")
     movieContainer.appendChild(movieImg)
     
-    //TODO overview
+    //* overview
     const moviePlot = document.createElement("p")
     moviePlot.textContent = movie.overview
     moviePlot.setAttribute("class", "moviePlot")
     movieContainer.appendChild(moviePlot)
     
-    //TODO release_date
+    //* release_date
     const movieRelease = document.createElement("p")
     movieRelease.textContent = movie.release_date;
     movieRelease.setAttribute("class", "movieRelease")
@@ -117,6 +161,15 @@ function createMovieObject(movie){
 
 
 //TODO Function that can save Movies to your watch list by stringifying it to localStorage, and on WindowLoaded should then decypher the data and create DOM elements in the WatchListContainer
+watchlistButton.addEventListener("click", function (event){
+    event.preventDefault()
+
+})
+
+
+
+
+
 async function errorMessage() {
     // switch (status.value) {
     //     case False:
